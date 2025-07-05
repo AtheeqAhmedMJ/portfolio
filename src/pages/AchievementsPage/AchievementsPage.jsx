38fa92fcefbf322a4ce7634c83d9ff6ad2265b2a
Mohamed Atheeq Ahmed M J <mdatheeq.2004@gmail.com>
2025-07-05 14:49:30 +0530

    responsive css

--- src/pages/AchievementsPage/AchievementsPage.jsx
+++ src/pages/AchievementsPage/AchievementsPage.jsx
@@ -7,6 +7,7 @@ import LightsaberCard from '../../components/LightsaberCard/LightsaberCard';
 const AchievementsPage = () => {
   return (
     <div className="achievements-page">
+      {/* Background Layers */}
       <div className="bg-image" />
       <div className="stars-bg">
         <div className="stars" />
@@ -14,11 +15,20 @@ const AchievementsPage = () => {
         <div className="stars3" />
       </div>
 
+      {/* Navigation */}
       <NavBar />
       <SideBar />
 
+      {/* Barcode Message */}
+      <div className="Barcode-Message">
+        YOU HAVE ARRIVED AT<br />
+        HALL OF HEROES - ACHIEVEMENTS
+      </div>
+
+      {/* Page Title */}
       <h1 className="achievements-title">ACHIEVEMENTS</h1>
 
+      {/* Lightsaber Cards */}
       <div className="lightsaber-card-container">
         <LightsaberCard
           title="Leadership & Community Impact"
@@ -38,10 +48,6 @@ const AchievementsPage = () => {
           glowColor="orange"
           to="/achievements/awards"
         />
-          <div className="Barcode-Message">
-           YOU HAVE ARRIVED AT<br />
-           HALL OF HEROES - ACHIEVEMENTS
-      </div>
       </div>
     </div>
   );
